ef386cede76140c74dbc923ca50884f7324524df
Upul Gunasena <forupul@yahoo.com>
2021-09-10 20:45:59 -0400

    Working version 2.00

--- picar_4wd/Cruising.py
+++ picar_4wd/Cruising.py
@@ -1,118 +1,26 @@
-import io
-import re
-
-import picamera
-from PIL import Image
-from tflite_runtime.interpreter import Interpreter
-
-CAMERA_WIDTH = 640
-CAMERA_HEIGHT = 480
-
-
-class HouserBoon:
-    args = {
-        "labels": "/home/pi/picar-4wd/coco_labels.txt",
-        "model": "/home/pi/picar-4wd/detect.tflite",
-        "threshold": .6
-    }
-
-    def load_labels(self, path):
-        with open(path, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-            labels = {}
-            for row_number, content in enumerate(lines):
-                pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
-                if len(pair) == 2 and pair[0].strip().isdigit():
-                    labels[int(pair[0])] = pair[1].strip()
-                else:
-                    labels[row_number] = pair[0].strip()
-        return labels
-
-    def __init__(self):
-        self.labels = self.load_labels(self.args["labels"])
-        self.interpreter = Interpreter(self.args["model"])
-        self.interpreter.allocate_tensors()
-        _, self.input_height, self.input_width, _ = self.interpreter.get_input_details()[0]['shape']
-
-    def set_input_tensor(self, interpreter, image):
-        """Sets the input tensor."""
-        tensor_index = interpreter.get_input_details()[0]['index']
-        input_tensor = interpreter.tensor(tensor_index)()[0]
-        input_tensor[:, :] = image
-
-    def get_output_tensor(self, interpreter, index):
-        """Returns the output tensor at the given index."""
-        output_details = interpreter.get_output_details()[index]
-        tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
-        return tensor
-
-    def detect_objects(self, interpreter, image, threshold):
-        """Returns a list of detection results, each a dictionary of object info."""
-        self.set_input_tensor(interpreter, image)
-        self.interpreter.invoke()
-
-        # Get all output details
-        boxes = self.get_output_tensor(self.interpreter, 0)
-        classes = self.get_output_tensor(self.interpreter, 1)
-        scores = self.get_output_tensor(self.interpreter, 2)
-        count = int(self.get_output_tensor(self.interpreter, 3))
-
-        results = []
-        for i in range(count):
-            if scores[i] >= threshold:
-                result = {
-                    'bounding_box': boxes[i],
-                    'class_id': classes[i],
-                    'score': scores[i]
-                }
-                results.append(result)
-        return results
-
-    def show_us_the_way(self):
-        with picamera.PiCamera(resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=10) as camera:
-            camera.start_preview()
-            try:
-                stream = io.BytesIO()
-                camera.capture(stream, format="jpeg")
-                stream.seek(0)
-                image = Image.open(stream).convert('RGB').resize(
-                    (self.input_width, self.input_height), Image.ANTIALIAS)
-
-                results = self.detect_objects(self.interpreter, image, self.args["threshold"])
-                for obj in results:
-                    # TO-DO Need to find why it is always detecting person
-                    if self.labels[obj['class_id']] == "person" or self.labels[obj['class_id']] == "stop sign":
-                        print("self.labels[obj['class_id']] ", self.labels[obj['class_id']])
-                        return self.labels[obj['class_id']]
-
-                stream.seek(0)
-                stream.truncate()
-
-            finally:
-                camera.stop_preview()
-        return "Clear"
-
-
-import numpy as np
 import math
 import time
-from picar_4wd.pin import Pin
-from picar_4wd.pwm import PWM
-from picar_4wd.ultrasonic import Ultrasonic
-from picar_4wd.servo import Servo
-from picar_4wd.speed import Speed
-import picar_4wd as fc
+
+import numpy as np
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 
+import picar_4wd as fc
+from DetectObjects import DetectObjects
+from picar_4wd.pin import Pin
+from picar_4wd.pwm import PWM
+from picar_4wd.servo import Servo
+from picar_4wd.speed import Speed
+from picar_4wd.ultrasonic import Ultrasonic
 
-class LightningMcqueen:
+
+class Cruising:
     ser = Servo(PWM("P0"))
     us = Ultrasonic(Pin("D8"), Pin("D9"))
     angle_increment = 2
     length_per_position = 5.5  # 5 cm / position in numpy array
-    houser_boon = HouserBoon()
+    detectObjects = DetectObjects()
     target = (150, 150)
     direction = 0
 
@@ -145,7 +53,6 @@ class LightningMcqueen:
         direction = self.direction
         prev = self.position
 
-
         # direction 0 = east
         # direction 1 = south
         # direction -1 = north
@@ -154,10 +61,10 @@ class LightningMcqueen:
             print("Moving to this location", coordinate)
             print("Moving to this direction", direction)
             move = self.findMove(coordinate, prev)
-            while (self.houser_boon.show_us_the_way() == "person"):
+            while (self.detectObjects.show_us_the_way() == "person"):
                 time.sleep(3)
                 print("STOP at a Person")
-            if (self.houser_boon.show_us_the_way() == "stop sign"):
+            if (self.detectObjects.show_us_the_way() == "stop sign"):
                 time.sleep(5)
                 print("STOP at a stop sign")
 
@@ -186,7 +93,7 @@ class LightningMcqueen:
                 tempList[0] += self.length_per_position
                 self.position = tuple(tempList)
 
-            if direction == 0 and move == "down":
+            elif direction == 0 and move == "down":
                 print("Down direction == 0")
                 self.turnRight()
                 self.move25()
@@ -250,10 +157,6 @@ class LightningMcqueen:
         self.target = tuple(tempTarget)
 
         self.direction = direction
-
-    def kachow(self):
-        while (self.lets_do_this_thing(self.map_to_fill)):
-            print("Scanned")
 
     def move25(self):
         speed4 = Speed(25)
@@ -330,6 +233,11 @@ class LightningMcqueen:
         return map_to_fill
 
 
-SPEED = LightningMcqueen((50, 75))
+def main():
+    car = Cruising(target=(50, 75))
+    while car.lets_do_this_thing(car.map_to_fill):
+        print("Scanned")
 
-SPEED.kachow()
+
+if __name__ == '__main__':
+    main()
